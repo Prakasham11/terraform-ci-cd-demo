f1719c3cb112b9e221b134379690c0faa4d482b9
Prakasham11 <137135852+Prakasham11@users.noreply.github.com>
2025-07-24 13:13:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "my-ci-cd-demo-bucket-123456"
+  force_destroy = true
+  tags = {
+    Name = "CI/CD Demo Bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "demo_bucket_acl" {
+  bucket = aws_s3_bucket.demo_bucket.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
